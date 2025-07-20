a54d6241eb0cebc1af74cdb9b1ec7059e416f6ba
carons02 <simon.j.caron@gmail.com>
2025-07-19 22:28:49 -0400

    Bosium Strip Code Editor Version 3.1 First Upload
    First Load of Bosium Strip Code Editor for 3.1. Future revisions will provide greater enhancements and necessary modifications for user engagement.

---
+++ Bosium Strip/Bosium Strip/BosiumStripForm.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace Bosium_Strip
+{
+    partial class BosiumStripForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BosiumStripMenu = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveAsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.findToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.TextBoxEditor = new System.Windows.Forms.RichTextBox();
+            this.openFileDialogMain = new System.Windows.Forms.OpenFileDialog();
+            this.MainStatusStrip = new System.Windows.Forms.StatusStrip();
+            this.BosiumStripStatusLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.saveFileDialogMain = new System.Windows.Forms.SaveFileDialog();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.BosiumStripMenu.SuspendLayout();
+            this.MainStatusStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // BosiumStripMenu
+            // 
+            this.BosiumStripMenu.BackColor = System.Drawing.SystemColors.Window;
+            this.BosiumStripMenu.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.BosiumStripMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.editToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.BosiumStripMenu.Location = new System.Drawing.Point(0, 0);
+            this.BosiumStripMenu.Name = "BosiumStripMenu";
+            this.BosiumStripMenu.Padding = new System.Windows.Forms.Padding(13, 2, 0, 2);
+            this.BosiumStripMenu.Size = new System.Drawing.Size(950, 24);
+            this.BosiumStripMenu.TabIndex = 0;
+            this.BosiumStripMenu.Text = "Menus";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newToolStripMenuItem,
+            this.openToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.saveAsToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // newToolStripMenuItem
+            // 
+            this.newToolStripMenuItem.Name = "newToolStripMenuItem";
+            this.newToolStripMenuItem.Size = new System.Drawing.Size(114, 22);
+            this.newToolStripMenuItem.Text = "New";
+            this.newToolStripMenuItem.Click += new System.EventHandler(this.newToolStripMenuItem_Click);
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(114, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(114, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // saveAsToolStripMenuItem
+            // 
+            this.saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
+            this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(114, 22);
+            this.saveAsToolStripMenuItem.Text = "Save As";
+            this.saveAsToolStripMenuItem.Click += new System.EventHandler(this.saveAsToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(114, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // editToolStripMenuItem
+            // 
+            this.editToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.findToolStripMenuItem});
+            this.editToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.editToolStripMenuItem.Name = "editToolStripMenuItem";
+            this.editToolStripMenuItem.Size = new System.Drawing.Size(39, 20);
+            this.editToolStripMenuItem.Text = "Edit";
+            // 
+            // findToolStripMenuItem
+            // 
+            this.findToolStripMenuItem.Name = "findToolStripMenuItem";
+            this.findToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.findToolStripMenuItem.Text = "Find";
+            this.findToolStripMenuItem.Click += new System.EventHandler(this.findToolStripMenuItem_Click);
+            // 
+            // TextBoxEditor
+            // 
+            this.TextBoxEditor.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.TextBoxEditor.Location = new System.Drawing.Point(0, 64);
+            this.TextBoxEditor.Margin = new System.Windows.Forms.Padding(5, 2, 5, 2);
+            this.TextBoxEditor.Name = "TextBoxEditor";
+            this.TextBoxEditor.Size = new System.Drawing.Size(950, 386);
+            this.TextBoxEditor.TabIndex = 1;
+            this.TextBoxEditor.Text = "";
+            // 
+            // openFileDialogMain
+            // 
+            this.openFileDialogMain.FileName = "openFileDialogMain";
+            // 
+            // MainStatusStrip
+            // 
+            this.MainStatusStrip.BackColor = System.Drawing.Color.Transparent;
+            this.MainStatusStrip.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.MainStatusStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.BosiumStripStatusLabel});
+            this.MainStatusStrip.Location = new System.Drawing.Point(0, 42);
+            this.MainStatusStrip.Name = "MainStatusStrip";
+            this.MainStatusStrip.Size = new System.Drawing.Size(950, 22);
+            this.MainStatusStrip.TabIndex = 3;
+            this.MainStatusStrip.Text = "statusStrip1";
+            // 
+            // BosiumStripStatusLabel
+            // 
+            this.BosiumStripStatusLabel.Name = "BosiumStripStatusLabel";
+            this.BosiumStripStatusLabel.Size = new System.Drawing.Size(109, 17);
+            this.BosiumStripStatusLabel.Text = "Bosium Strip Status";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem,
+            this.helpToolStripMenuItem1});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.aboutToolStripMenuItem.Text = "About Editor";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // helpToolStripMenuItem1
+            // 
+            this.helpToolStripMenuItem1.Name = "helpToolStripMenuItem1";
+            this.helpToolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            this.helpToolStripMenuItem1.Text = "Help...";
+            this.helpToolStripMenuItem1.Click += new System.EventHandler(this.helpToolStripMenuItem1_Click);
+            // 
+            // BosiumStripForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(950, 450);
+            this.Controls.Add(this.MainStatusStrip);
+            this.Controls.Add(this.TextBoxEditor);
+            this.Controls.Add(this.BosiumStripMenu);
+            this.Font = new System.Drawing.Font("Lucida Console", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MainMenuStrip = this.BosiumStripMenu;
+            this.Margin = new System.Windows.Forms.Padding(5, 2, 5, 2);
+            this.Name = "BosiumStripForm";
+            this.Text = "Bosium Strip";
+            this.Load += new System.EventHandler(this.BosiumStripForm_Load);
+            this.BosiumStripMenu.ResumeLayout(false);
+            this.BosiumStripMenu.PerformLayout();
+            this.MainStatusStrip.ResumeLayout(false);
+            this.MainStatusStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip BosiumStripMenu;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveAsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
+        private System.Windows.Forms.RichTextBox TextBoxEditor;
+        private System.Windows.Forms.OpenFileDialog openFileDialogMain;
+        private System.Windows.Forms.StatusStrip MainStatusStrip;
+        private System.Windows.Forms.ToolStripStatusLabel BosiumStripStatusLabel;
+        private System.Windows.Forms.ToolStripMenuItem findToolStripMenuItem;
+        private System.Windows.Forms.SaveFileDialog saveFileDialogMain;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem1;
+    }
+}
+
---
+++ Bosium Strip/Bosium Strip/BosiumStripForm.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Bosium_Strip
+{
+    public partial class BosiumStripForm : System.Windows.Forms.Form
+    {
+        // Create a member class variable to track the current file path
+        private string m_currentFilePath = string.Empty;
+        private string m_currentFileName = string.Empty;
+
+        public BosiumStripForm()
+        {
+            //This will run the VS code and VS created/wrote.
+            InitializeComponent();
+
+            //Created custom styling for the Form after its made
+            SetupStyling();
+
+        }
+
+        private void BosiumStripForm_Load(object sender, EventArgs e)
+        {
+            //Code for setting up dialog box filters for text code files
+            openFileDialogMain.Filter = "Text Files (*.txt)|*.txt|C# Console Files(*.cs)|*.cs|Python Files (*.py)|*.py|All Files (*.*)|*.*";
+            saveFileDialogMain.Filter = "Text Files (*.txt)|*.txt|C# Console Files(*.cs)|*.cs|Python Files (*.py)|*.py|All Files (*.*)|*.*";
+
+            //Starts the cursor in the text box editor
+            TextBoxEditor.Focus();
+        }
+        //Code area for creating custom styling for form
+        private void SetupStyling()
+        {
+            // Color set for overall Form
+            this.BackColor = Color.LightCyan;
+
+            // Color set for the Menu strip, both front and back
+            BosiumStripMenu.BackColor = Color.Black;
+            BosiumStripMenu.ForeColor = Color.Cyan;
+            BosiumStripMenu.Font = new Font("Lucida Console", 9);
+            fileToolStripMenuItem.Font = new Font("Lucida Console", 9);
+            editToolStripMenuItem.Font = new Font("Lucida Console", 9);
+
+            // Custom styling for the Text Box: colors, font, wordwrap
+            TextBoxEditor.BackColor = Color.DarkGray;
+            TextBoxEditor.ForeColor = Color.White;
+            TextBoxEditor.Font = new Font("Lucida Console", 11, FontStyle.Bold);
+            TextBoxEditor.AcceptsTab = true;
+            TextBoxEditor.WordWrap = true;
+
+            // Custom styling for the status strip
+            MainStatusStrip.BackColor = Color.Silver;
+            MainStatusStrip.ForeColor = Color.Blue;
+            MainStatusStrip.Font = new Font("Lucida Console", 11);
+            MainStatusStrip.Text = "Input your code area...";
+
+            // Expand the window title
+            this.Text = "Bosium Strip Editor";
+        }
+
+        private void newToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // The user raised the new event. need to handle it by created a new code file
+            // Clear the current text box
+            TextBoxEditor.Clear();
+            // clear our current file path
+            m_currentFilePath = string.Empty;
+            // setting the current file
+            this.Text = "Bosium Strip Editor - New File";
+            //let the user know what is going on
+            BosiumStripStatusLabel.Text = "New File Created";
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // The user raised the exit event. Need to close the program
+            Application.Exit();
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // The user raised the open event. Need to handle the open function
+            if (openFileDialogMain.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    //Try to read the user selected file and put it in our editor
+                    string fileContent = File.ReadAllText(openFileDialogMain.FileName);
+                    TextBoxEditor.Text = fileContent;
+                    m_currentFilePath = openFileDialogMain.FileName;
+                    this.Text = "Bosium Strip Editor - " + Path.GetFileName(openFileDialogMain.FileName);
+                    BosiumStripStatusLabel.Text = "File Opened: " + Path.GetFileName(openFileDialogMain.FileName);
+                }
+                catch(Exception ex) 
+                {
+                    //If Trying to open an existing file doesn't work, an event is raised notifying the user which is handled here
+                    MessageBox.Show("The file you were trying to open is not available. " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // The user raised the save event. Need to handle saving the file
+            //If the current file path is empty, it runs through the script for user action and input
+            if(string.IsNullOrEmpty(m_currentFilePath))
+            {
+                //show a dialog to choose where to save this current code file in the editor.
+                if (saveFileDialogMain.ShowDialog() == DialogResult.OK)
+                {
+                    //sets current file path equal to the file name
+                    m_currentFilePath = saveFileDialogMain.FileName;
+                }
+                else
+                {
+                    //If we are here, our user cancelled the save and we do nothing
+                    return;
+                }
+            }
+            try
+            {
+                //Try to read the user defined save path to save the file
+                File.WriteAllText(m_currentFilePath, TextBoxEditor.Text);
+                this.Text = "Bosium Strip Editor - " + Path.GetFileName(m_currentFilePath);
+                BosiumStripStatusLabel.Text = "File saved: " + Path.GetFileName(m_currentFilePath);
+            }
+            // If there is an error raised by the system by attempting to save, it's handled here
+            catch (Exception ex)
+            {
+                //show an messagebox to our user if there is an error with saving the file
+                MessageBox.Show("Error saving the text from the editor to a file: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error); return;
+            }
+        }
+
+        private void saveAsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // The user raised the saveas event. Need to save the file as the user wishes to name it, both the current name or new name
+            try
+            {
+                //Give the user the option to save the text file in a new or current location
+                File.WriteAllText(saveFileDialogMain.FileName, TextBoxEditor.Text);
+                m_currentFilePath = saveFileDialogMain.FileName;
+                this.Text = "Bosium Strip Editor - " + Path.GetFileName(m_currentFilePath);
+                BosiumStripStatusLabel.Text = "File saved as: " + Path.GetFileName(m_currentFilePath);
+            }
+            // If there is an error raised by the system by attemplting to save as, it's handled here
+            catch (Exception ex)
+            {
+                //show an messagebox to our user if there is an error with saving the file
+                MessageBox.Show("Error in save as process from the editor to a file: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error); return;
+            }
+        }
+
+        private void findToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //this will handle the find sercie in our code editor
+            // First let's ask our user what text are you looking for?
+            string searchText = Microsoft.VisualBasic.Interaction.InputBox("Enter text to find:", "Find", "");
+
+            // This checks if the user entered something to search for
+            if (!string.IsNullOrEmpty(searchText))
+            {
+                // This searches for the text in the rich text box
+                int index = TextBoxEditor.Find(searchText, RichTextBoxFinds.None);
+
+                if (index >= 0)
+                {
+                    // let's highlight the found text
+                    TextBoxEditor.SelectionStart = index;
+                    TextBoxEditor.SelectionLength = searchText.Length;
+                    TextBoxEditor.Focus();
+                    TextBoxEditor.Text = "Found: " + searchText;
+                }
+                else
+                {
+                    // This tells the user if the text wasn't found
+                    BosiumStripStatusLabel.Text = "Text not found: " + searchText;
+                    MessageBox.Show("Text not found!", "Find", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //This tells the user background information about the code editor.
+            MessageBox.Show("Developed in MS539 for use on future coding as needed." +
+                Environment.NewLine + Environment.NewLine + "Circa 16 July, 2025.","About", MessageBoxButtons.OK);
+
+        }
+
+        private void helpToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            //This provides information for the user regarding any help they are hoping to get while using the editor.
+            MessageBox.Show("Yeah good luck with that right now." + Environment.NewLine + Environment.NewLine +
+                "Maybe try Youtube if you need help." + Environment.NewLine + Environment.NewLine +
+                "Hopefully in the future there's more here for you", "Get Help", MessageBoxButtons.OK);
+        }
+    }//this is the of our class scope formMain
+}//this is the end of our namespace scope Bosium Strip
